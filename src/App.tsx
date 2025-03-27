4ce76a97b39002abc5ddca29c9a1390347920609
harrychen1995 <harrychen1995@yahoo.com>
2025-03-27 10:06:28 -0500

    project added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TripiePlanningAI from './landingPage';
-
 function App() {
   return (
     <div>
--- src/landingPage/index.tsx
+++ src/landingPage/index.tsx
@@ -9,7 +9,7 @@ import AboutUs from "../About";
 
 const TripiePlanningAI: React.FC = () => {
   return (
-<Router>
+<Router basename={process.env.PUBLIC_URL}>
       <div className="d-flex flex-column justify-content-center min-vh-100">
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark text-light mb-4 flex-grow-3">
           <div className="container">
